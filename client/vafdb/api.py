deca36939c9850f396e12b6a8554d1067b43bc75
tombch <tombrier@hotmail.co.uk>
2022-12-08 14:53:57 +0000

    client-side deletion logic

--- client/vafdb/api.py
+++ client/vafdb/api.py
@@ -11,11 +11,12 @@ class Client:
             "create": f"{self.url}/data/",
             "get": f"{self.url}/data/",
             "query": f"{self.url}/data/query/",
+            "delete": lambda x: f"{self.url}/data/{x}",
         }
 
     def create(self, fields):
         """
-        Post a metadata record to `VAFDb`.
+        Post a metadata record to `vafdb`.
         """
         response = requests.post(
             self.endpoints["create"],
@@ -25,7 +26,7 @@ class Client:
 
     def csv_create(self, csv_path: str, delimiter: str | None = None):
         """
-        Post a .csv or .tsv containing multiple metadata records to `VAFDb`.
+        Post a .csv or .tsv containing multiple metadata records to `vafdb`.
         """
         if csv_path == "-":
             csv_file = sys.stdin
@@ -49,7 +50,7 @@ class Client:
 
     def get(self, fields=None):
         """
-        Retrieve data from `VAFDb`.
+        Retrieve data from `vafdb`.
         """
         if fields is None:
             fields = {}
@@ -73,7 +74,7 @@ class Client:
 
     def query(self, query):
         """
-        Retrieve data from `VAFDb`.
+        Retrieve data from `vafdb`.
         """
         if not isinstance(query, Field):
             raise Exception("Query must be of type Field")
@@ -94,3 +95,35 @@ class Client:
                 _next = response.json()["next"]
             else:
                 _next = None
+
+    def delete(self, sample_id):
+        """
+        Suppress data in `vafdb`.
+        """
+        response = requests.delete(self.endpoints["delete"](sample_id))
+        return response
+
+    def csv_delete(self, csv_path, delimiter=None):
+        """
+        Use a .csv or .tsv to delete data in `vafdb`.
+        """
+        if csv_path == "-":
+            csv_file = sys.stdin
+        else:
+            csv_file = open(csv_path)
+        try:
+            if delimiter is None:
+                reader = csv.DictReader(csv_file)
+            else:
+                reader = csv.DictReader(csv_file, delimiter=delimiter)
+
+            for record in reader:
+                sample_id = record.get("sample_id")
+                if sample_id is None:
+                    raise KeyError("sample_id column must be provided")
+
+                response = requests.delete(self.endpoints["delete"](sample_id))
+                yield response
+        finally:
+            if csv_file is not sys.stdin:
+                csv_file.close()
